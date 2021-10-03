e0d907cb14045b1990676d330bbf2f8438bee4fb
Henrik Lissner <henrik@lissner.net>
2021-10-03 19:37:56 +0200

    refactor: simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -5,13 +5,9 @@ with pkgs;
 mkShell {
   buildInputs = [ ruby ];
   shellHook = ''
-    export PATH=./.gem/bin:"$PATH"
-    export GEM_HOME=./.gem
-    export SASS_PATH=.:./stylesheets
-
-    if ! command -v discourse_theme >/dev/null; then
-      echo "Installing discourse_theme CLI"
-      gem install discourse_theme
-    fi
+    mkdir -p .gem
+    export GEM_HOME=$PWD/.gem
+    export GEM_PATH=$GEM_HOME
+    export PATH=$GEM_HOME/bin:$PATH
   '';
 }
